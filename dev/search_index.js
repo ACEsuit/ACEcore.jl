6d3d192a046b4fefa7e1417e7dc7cac9be6fe132
Documenter.jl <documenter@juliadocs.github.io>
2022-09-28 04:06:01 +0000

    build based on a5aaf6d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ACEcore","category":"page"},{"location":"#ACEcore","page":"Home","title":"ACEcore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ACEcore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ACEcore]","category":"page"}]
+}

e30fbad6233b28617b78ac1673001f62e9b293d2
cortner <christohortner@gmail.com>
2022-09-28 18:11:37 -0700

    benchmark temps for prodbasis

--- src/prodbasis.jl
+++ src/prodbasis.jl
@@ -1,4 +1,4 @@
-
+using LoopVectorization
 
 # TODO: rename ProdBasis -> Sparse Symmetric Tensor Product 
 
@@ -51,3 +51,37 @@ function evaluate!(AAdag, dag::AAEvalGraph, A)
 
    return AAdag
 end
+
+
+# ---------------------------------------------------------------------
+
+import Polynomials4ML: TempArray, ArrayCache, acquire!, release!
+
+struct ProdBasis2{T}
+   dag::AAEvalGraph
+   pool_AA::ArrayCache{T, 1}
+   tmp_dag::TempArray{T, 1}
+end
+
+
+function ProdBasis2(spec::AbstractVector{<: AbstractVector}; T = Float64, kwargs...)
+   dag = AAEvalGraph(spec; kwargs...)
+   return ProdBasis2(dag, ArrayCache{T, 1}(), TempArray{T, 1}() )
+end
+
+
+(basis::ProdBasis2)(A::AbstractVector) = evaluate(basis, A)
+
+function evaluate(basis::ProdBasis2, A::AbstractVector{T}) where {T} 
+   AAdag_ = acquire!(basis.tmp_dag, length(basis.dag), T)
+   AAdag = parent(AAdag_)
+   AA_ = acquire!(basis.pool_AA, length(basis.dag.projection), T)
+   AA = parent(AA_)
+   evaluate!(AAdag, basis.dag, A)
+   @inbounds for i = 1:length(basis.dag.projection)
+      AA[i] = AAdag[basis.dag.projection[i]]
+   end
+   return AA_ 
+   # return AAdag[basis.dag.projection]
+end
+
--- test/test_prodbasis1.jl
+++ test/test_prodbasis1.jl
@@ -1,7 +1,6 @@
 
 using ACEcore, BenchmarkTools
-using ACEcore: SimpleProdBasis
-using ACEcore: ProdBasis
+using ACEcore: SimpleProdBasis, ProdBasis, ProdBasis2
 
 ##
 
@@ -17,7 +16,23 @@ AA1 = basis1(A)
 basis2 = ProdBasis(spec)
 AA2 = basis2(A)
 
+basis3 = ProdBasis2(spec; T = ComplexF64)
+AA3 = basis3(A)
+
 @btime $basis1($A)
 @btime $basis2($A)
+@btime $basis3($A)
+@btime (AA = $basis3($A); ACEcore.release!(AA))
 
-AA1 ≈ AA2
+AA1 ≈ AA2 ≈ AA3 
+
+
+##
+
+@profview let basis = basis3, A = A 
+   for n = 1:1000_000 
+      AA = basis(A)
+      ACEcore.release!(AA)
+   end
+end
+

9586ae49dfd9452441bfb11623b8fa365a5e28b3
cortner <christohortner@gmail.com>
2022-10-13 23:25:20 -0400

    Enzyme example for 1pbasi

--- profile/profile_1pbasis.jl
+++ profile/profile_1pbasis.jl
@@ -34,5 +34,48 @@ A4 = ACEcore.test_evalpool(basis, bBB)
 
 @assert A1 ≈ A2 ≈ A3 ≈ A4
 
-@btime serial_evalpool!($A1, $At, $basis, $bBB)
+@btime serial_evalpool!($A1, $basis, $bBB)
 @btime ACEcore.evalpool!($A2, $basis, $bBB)
+
+## 
+
+∂A = randn(size(A1))
+val, pb = ACEcore._rrule_evalpool(basis, bBB)
+∂BB = pb(∂A)
+
+@btime begin 
+   val, pb = ACEcore._rrule_evalpool($basis, $bBB)
+   ∂BB = pb($∂A)
+end
+
+##
+
+
+using Enzyme, LinearAlgebra
+
+∂BBe = map(zero, bBB)
+Ae = zero(A1)
+
+autodiff(Reverse, ACEcore.evalpool!, Const, Duplicated(Ae, ∂A), Const(basis), 
+         Duplicated(bBB, ∂BBe))
+
+@show Ae ≈ val         
+@show all(∂BBe .≈ ∂BB)
+
+@btime begin 
+   autodiff(Reverse, ACEcore.evalpool!, Const, Duplicated($Ae, $∂A), Const($basis), 
+                Duplicated($bBB, $∂BBe))
+end
+
+
+# ##
+
+# using Enzyme
+
+# BB = ( randn(nX, N1), randn(nX, N2), randn(nX, N3) )
+# ∂BB = map(zero, BB)
+
+# A = zeros(100)
+# dA = ones(100)
+
+# autodiff(Reverse, ACEcore.evalpool!, Const, Duplicated(A, dA), Const(basis), Duplicated(BB, ∂BB))
